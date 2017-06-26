767a5e2f0c2d5905f01fe2569f328f257a8aa120
JulioAndresPadillaCruz <julioandres.padilla@gmail.com>
2017-06-26 01:14:14 -0500

    Agregados los cambios del chat

--- public/chat-admin/chat-admin.component.js
+++ public/chat-admin/chat-admin.component.js
@@ -2,7 +2,88 @@ angular.
 	module('chatAdmin').
 		component('chatAdmin', {
 			templateUrl: 'chat-admin/chat-admin.template.html',
-			controller: function Chat(){
-				agregarEventos();
+			controller: function chatAdminController(){
+				var self = this;
+
+				self.openChat = function(id){
+					$('.conversation#chat-'+id).slideToggle(300); //QUe tenga la clase y el id, 'chat-'+id
+					//$('#'+id).slideToggle(300); //Podria ser solo el id
+				}
+
+				self.agregarEventos = function(){
+					$('.js-trigger').on('click', function(){
+					    $('html').toggleClass('show-me');
+					});
+					$('.conversation__header').on('click', function(){
+						$('.conversation').slideToggle(300);
+					});
+
+					//$('.js-trigger').on('click', function(){
+					    //$('.conversation').slideToggle(300);
+						//$('.chat').animate({scrollTop: $('.chat')[0].scrollHeight}, 300);//MUevo el scroll hasta abajo pero despues de 300 milisegundos, que es cuando el chat se abrio completamente y el scroll es del tamaño que debe
+					//});
+					var socket = io(); //Al ser la página principal creo el socket
+				
+					var oldDocs = '';
+					//var socket = io(); //SOcket nuevo//SI lo pongo aquí cada que se importe el chat se creara un nuevo socket con evenos asociados, por lo tanto repetira acciones del socket anterior(Si es que llegara a existir)
+					//var socket = io.connect('http://localhost:8000', {'forceNew': true});
+					
+					socket.on('load old msgs', function(docs){
+						//$('.conversation').slideToggle(300);
+						//alert("viejo");
+						oldDocs = docs;
+						cargarOldMensajes();
+					});
+					socket.off('new message');
+					socket.on('new message',function(data){
+							//alert("new message");
+							AppendNewMsg(data);
+						});		
+
+					
+					function cargarOldMensajes(){
+						$.each(oldDocs,function(index, data){// data: objeto del array oldDocs, con campos: msg, sender, reciever, nickname
+							AppendNewMsg(data);
+						});	
+						socket.off('load old msgs'); //QUitar la escucha del evento//Si no lo quitamos cada que se conecte alguien al servidor(socket) nuestro chat dibujara otra vez los chats guardados en la BD(duplicando, triplicando, etc.. los chats)
+					}
+					function AppendNewMsg(data){
+							if(self.currentUserId == data.reciever){
+								//register_popup(data.nickname, data.nickname); //nickname = nickname de quien lo mandó
+							}
+							//Llenamos con mensajes el popup
+							$.each($('.chat'),function(index,datax){ //Para cada caja de mensajes, auqellas que tengan su atributo chatboxid == data.reciever || chatboxid == data.sender
+								if($(datax).attr('chatboxid')==data.reciever || $(datax).attr('chatboxid')==data.sender){//El mensaje me ivolucra?
+									if(self.currentUserId == data.reciever){//Sí, te lo enviaron
+										//$(datax).append("</br><div class='recieve'><b>"+data.nickname+": </b>"+data.msg+"</div>");
+										$(datax).append("<li class='conversation__msg cf'>"+"<span>"+data.msg+"</span></li>");
+										//$(datax).animate({scrollTop: $(datax)[0].scrollHeight}, 2000);
+										$(datax).animate({scrollTop: $(datax)[0].scrollHeight}, 0);
+									}else if( self.currentUserId == data.sender){ //Sí, lo enviaste
+										//$(datax).append("</br><div class='send'><b>"+data.nickname+": </b>"+data.msg+"</div>");
+										$(datax).append("<li class='conversation__msg cf'>"+"<span class='right'>"+data.msg+"</span></li>");
+										//$(datax).animate({scrollTop: $(datax)[0].scrollHeight}, 2000);
+										$(datax).animate({scrollTop: $(datax)[0].scrollHeight}, 0);
+									}
+								}
+							});
+						}
+
+						self.currentUserId = localStorage.getItem("email");
+
+
+					$(document).on( "keypress",'input.message', function(event) {//CUando document esté cargado
+						var $messageBox = $(this); //this hace referencia al elemento que disparo el evento, el input en este caso
+						if (event.which == 13 && !event.shiftKey && $messageBox.val()!= '' ){			
+							//event.preventDefault();//LO quito porque este no es de tipo submit
+							var $reciever = $(this).attr('chatboxid');
+							//socket.emit('send message', {msg:$messageBox.val(),sender: $currentUserId, reciever:$reciever});
+							socket.emit('send message', {msg:$messageBox.val(),sender: self.currentUserId, reciever:$reciever});
+							$messageBox.val('');
+						}
+					});
+
+				}
+				self.agregarEventos();
 			}
 	});
--- server.js
+++ server.js
@@ -5,6 +5,31 @@ var mongoose = require('mongoose');
 var db = require('./config/db');
 var app = express();
 var port = 8000;
+//Variables para chat
+//var express= require('express'),
+//	app=express(),
+server = require('http').createServer(app);
+io = require('socket.io').listen(server);
+//mongoose = require('mongoose'),
+
+	users={};
+
+	//Middlewares
+	app.use(bodyParser.urlencoded({ extended: false }));
+	app.use(bodyParser.json());
+	app.use(methodOverride());
+	
+	//Connection to BD (Conexion directa)
+	//mongoose.connect(db.url);
+
+	//COnecction to BD(Conexion con mensaje)
+	//var mongoURI = "mongodb://localhost/chatapp";
+	var mongoURI = db.url;
+	var MongoDB = mongoose.connect(mongoURI).connection;
+	MongoDB.on('error', function(err) { console.log(err.message); });
+	MongoDB.once('open', function() {
+		console.log("mongodb connection open");
+	});
 
 var users_routes = require('./routes/users-api/users.routes'); //Routes y app.use(...) //NOta: no se le indica su extensión (.js)
 var UserModel = require('./routes/users-api/users.schema'); //Instancio el codigo 'users.schema.js' en la variable UserModel, el codigo de 'users.schema.js' registra un schema al modelo 'User', para que posteriormente dicho modelo se pueda utilizar en el controlador (Busquedas sobre ese modelo base)
@@ -26,19 +51,6 @@ var internet_routes = require('./routes/internet-api/internet.routes');
 var InternetModel = require('./routes/internet-api/internet.schema'); 
 var InternetCtrl = require('./routes/internet-api/internet.controller');
 
-//Connection to BD
-mongoose.connect(db.url);
-
-//Middlewares
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(methodOverride());
-
-//IMPORTANTE! vamos a DEFINIR donde vamos a SERVIR la pagina web del FRONT END, index.html, style.css, bootstrap, angular, etc..
-//app.use(express.static(__dirname + '/public')); //Igual funciona
-app.use(express.static('public')); //Para el servicio de archivos estáticos como, por ejemplo, imágenes, archivos CSS y archivos JavaScript, utilice la función de middleware incorporado express.static de Express.
-
-
 //API routes
 users_routes.addAPIRouter(app, UserCtrl); //La funcion addAPIRouter enlaza rutas de un api que creamos a sus controladores(funciones); y hace que 'app' use esas rutas(app.use())
 //API routes_Reportes
@@ -48,7 +60,58 @@ servSoftwRoutes.addAPIRouter(app, servSoftwCtrl);
 //Api internet_service
 internet_routes.addAPIRouter(app, InternetCtrl);
 
-//Start Server
-app.listen(port, function(){
-	console.log("Node server running on http://localhost:" + port);
-});
+	var chatSchema = mongoose.Schema({
+		sender: String,
+		nickname: String,
+		reciever: String,
+		msg: String,
+		created: {type: Date, default: Date.now}
+	});
+	
+	var ChatModel = mongoose.model('Message',chatSchema);
+
+		
+
+//IMPORTANTE! vamos a DEFINIR donde vamos a SERVIR la pagina web del FRONT END, index.html, style.css, bootstrap, angular, etc..
+//app.use(express.static(__dirname + '/public')); //Igual funciona
+	app.use(express.static('public')); //Para el servicio de archivos estáticos como, por ejemplo, imágenes, archivos CSS y archivos JavaScript, utilice la función de middleware incorporado express.static de Express.
+
+	io.on('connection',function(socket){ //socket se crea cada que alguien se conecta, es el mismo proceso/funciones para cada usuario que se conecta
+		console.log('new connection done');
+		
+		ChatModel.find({}, function(err, docs){
+			if(err)throw err;
+			console.log('sending old msgs');
+			io.emit('load old msgs', docs);
+			//console.log(docs);
+		});
+
+		socket.on('send message',function(data){
+			console.log("Received message");
+			var newMsg = new ChatModel({msg:data.msg, sender: data.sender, reciever: data.reciever,nickname:socket.nickname});
+			newMsg.save(function(err){
+				if(err){
+					throw err;
+				}else{
+					console.log("Reply message");
+					io.emit('new message',{msg:data.msg,sender: data.sender, reciever: data.reciever,nickname:socket.nickname});
+				}
+			});			
+		});
+		
+
+		socket.on('disconnect', function(data){
+			if(!socket.nickname) return;
+			delete users[socket.nickname];
+			updateNicknames();
+		});
+	});
+
+	//Start Server socket.io
+	server.listen(port, function(){ //AQUI ESTABA EL ERROR, PARA socket.io ahora se hará el listen sobre server, NO sobre app
+		console.log("Node server running on http://localhost:" + port);
+	});
+	//Start Server
+	//app.listen(port, function(){
+	//	console.log("Node server running on http://localhost:" + port);
+	//});
